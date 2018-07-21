4f267ca401a64a8d0e4511a38c29bf5516eea4eb
Mortuza <mortuzahossain1997@gmail.com>
2018-07-22 00:31:59 +0600

    Remove Default Media Player And Implement ExoPlayer.

--- app/src/main/java/com/example/mortuza/radioplayer/Player.java
+++ app/src/main/java/com/example/mortuza/radioplayer/Player.java
@@ -1,54 +1,76 @@
 package com.example.mortuza.radioplayer;
 
-import android.media.MediaPlayer;
+
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.LinearInterpolator;
 import android.view.animation.RotateAnimation;
 import android.widget.Button;
-import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.chibde.visualizer.BarVisualizer;
+import com.google.android.exoplayer2.ExoPlaybackException;
+import com.google.android.exoplayer2.ExoPlayer;
+import com.google.android.exoplayer2.ExoPlayerFactory;
+import com.google.android.exoplayer2.PlaybackParameters;
+import com.google.android.exoplayer2.SimpleExoPlayer;
+import com.google.android.exoplayer2.Timeline;
+import com.google.android.exoplayer2.extractor.DefaultExtractorsFactory;
+import com.google.android.exoplayer2.extractor.ExtractorsFactory;
+import com.google.android.exoplayer2.source.ExtractorMediaSource;
+import com.google.android.exoplayer2.source.MediaSource;
+import com.google.android.exoplayer2.source.TrackGroupArray;
+import com.google.android.exoplayer2.trackselection.AdaptiveTrackSelection;
+import com.google.android.exoplayer2.trackselection.DefaultTrackSelector;
+import com.google.android.exoplayer2.trackselection.TrackSelection;
+import com.google.android.exoplayer2.trackselection.TrackSelectionArray;
+import com.google.android.exoplayer2.trackselection.TrackSelector;
+import com.google.android.exoplayer2.upstream.BandwidthMeter;
+import com.google.android.exoplayer2.upstream.DataSource;
+import com.google.android.exoplayer2.upstream.DefaultBandwidthMeter;
+import com.google.android.exoplayer2.upstream.DefaultDataSourceFactory;
+import com.google.android.exoplayer2.util.Util;
 import com.squareup.picasso.Picasso;
-
-import java.io.IOException;
 
 import de.hdodenhof.circleimageview.CircleImageView;
 
 public class Player extends AppCompatActivity {
 
-    Button playBtn;
-    CircleImageView circleImageView;
-    TextView name;
-    ProgressBar progressBar;
-    static BarVisualizer barVisualizer;
+    private Button startButton;
+    private Button stopButton;
 
-    String streamUrlVal;
-
-    // For Media Player
-    static MediaPlayer player;
+    private SimpleExoPlayer player;
+    private BandwidthMeter bandwidthMeter;
+    private ExtractorsFactory extractorsFactory;
+    private TrackSelection.Factory trackSelectionFactory;
+    private TrackSelector trackSelector;
+    private DefaultBandwidthMeter defaultBandwidthMeter;
+    private DataSource.Factory dataSourceFactory;
+    private MediaSource mediaSource;
     boolean flag = true;
+
+    public Player() {
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_player);
 
-        circleImageView = findViewById(R.id.image);
-        playBtn = findViewById(R.id.btnPlay);
-        name = findViewById(R.id.name);
-        progressBar = findViewById(R.id.progressBar1);
-        barVisualizer = findViewById(R.id.barVisualizer);
+        CircleImageView circleImageView = findViewById(R.id.image);
+        Button playBtn = findViewById(R.id.btnPlay);
+        TextView name = findViewById(R.id.name);
 
         // Receiving Data From Intent
         String nameVal = getIntent().getStringExtra("NAME");
         String imageUrlVal = getIntent().getStringExtra("IMAGE");
-        streamUrlVal = getIntent().getStringExtra("STREAMURL");
+        String streamUrlVal = getIntent().getStringExtra("STREAMURL");
 
         // Setting The Name in the text view
         name.setText(nameVal);
@@ -68,91 +90,49 @@ public class Player extends AppCompatActivity {
         rotateAnimation.setRepeatCount(Animation.INFINITE);
         circleImageView.startAnimation(rotateAnimation);
 
+        bandwidthMeter = new DefaultBandwidthMeter();
+        extractorsFactory = new DefaultExtractorsFactory();
+        trackSelectionFactory = new AdaptiveTrackSelection.Factory(bandwidthMeter);
+        trackSelector = new DefaultTrackSelector(trackSelectionFactory);
+
+        defaultBandwidthMeter = new DefaultBandwidthMeter();
+        dataSourceFactory = new DefaultDataSourceFactory(this,
+                Util.getUserAgent(this, "mediaPlayerSample"), defaultBandwidthMeter);
+
+        mediaSource = new ExtractorMediaSource(Uri.parse(streamUrlVal), dataSourceFactory, extractorsFactory, null, null);
+
+        player = ExoPlayerFactory.newSimpleInstance(this, trackSelector);
+        player.prepare(mediaSource);
 
 
-        // Change The Progress Bar
-        progressBar.setMax(100);
-        progressBar.setVisibility(View.INVISIBLE);
-        progressBar.setIndeterminate(true);
-
-
-        // Initialize music player
-        initializeMediaPlayer();
-        startPlaying();
-
-
-        // Player Control For Player
         playBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 if (flag) {
-                    stopPlaying();
+                    stopPlayer();
                     flag = false;
                 } else {
-                    startPlaying();
+                    startPlayer();
                     flag = true;
                 }
             }
         });
 
+        startPlayer();
 
     }
 
-    private void initializeMediaPlayer() {
-        // Change The Button Visualizer
-        barVisualizer.setColor(ContextCompat.getColor(this, R.color.colorAccent));
-        barVisualizer.setDensity(30);
-
-        player = new MediaPlayer();
-        try {
-            player.setDataSource(streamUrlVal);
-        } catch (IllegalArgumentException e) {
-            e.printStackTrace();
-        } catch (IllegalStateException e) {
-            e.printStackTrace();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
-        player.setOnBufferingUpdateListener(new MediaPlayer.OnBufferingUpdateListener() {
-            public void onBufferingUpdate(MediaPlayer mp, int percent) {
-                progressBar.setIndeterminate(false);
-                progressBar.setSecondaryProgress(100);
-            }
-        });
+    private void stopPlayer() {
+        player.setPlayWhenReady(false);
     }
 
-    private void startPlaying() {
-        playBtn.setBackgroundResource(R.drawable.stop);
-        try {
-            barVisualizer.setPlayer(player.getAudioSessionId());
-            progressBar.setVisibility(View.VISIBLE);
-            player.prepareAsync();
-            player.setOnPreparedListener(new MediaPlayer.OnPreparedListener() {
-                public void onPrepared(MediaPlayer mp) {
-                    player.start();
-                }
-            });
-        } catch (Exception e) {
-            Log.d("vg", "Visualization Error.");
-        }
-    }
-
-    private void stopPlaying() {
-        playBtn.setBackgroundResource(R.drawable.play);
-        if (player.isPlaying()) {
-            player.stop();
-            player.release();
-            initializeMediaPlayer();
-            progressBar.setVisibility(View.INVISIBLE);
-            progressBar.setIndeterminate(true);
-        }
+    private void startPlayer() {
+        player.setPlayWhenReady(true);
     }
 
     @Override
-    protected void onPause() {
-        player.stop();
-        player.release();
-        finish();
-        super.onPause();
+    protected void onDestroy() {
+        super.onDestroy();
+        player.setPlayWhenReady(false);
     }
 }
